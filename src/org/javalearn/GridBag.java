1989b98f785bc6d8277371e7d128972bc1a475fb
prox3m <doronindima997@gmail.com>
2025-07-03 10:48:46 +0500

    1

--- src/org/javalearn/GridBag.java
+++ src/org/javalearn/GridBag.java
@@ -55,26 +55,33 @@ public class GridBag {
         
         frame.setVisible(true);
     }
-
-    public static void CALC(){
-        final JFrame frame = new JFrame("Калькулятор");
+    
+    public static final JFrame frame = new JFrame("Калькулятор");
+    
+    public static JButton button;
+    public static final GridBagConstraints c = new GridBagConstraints();
+    
+    public static void SetFrameSettings(){
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLayout(new GridBagLayout());
         frame.setSize(300, 500);
         frame.setLocationRelativeTo(null);
-        
-        JButton button;
-        frame.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-        c.fill = GridBagConstraints.HORIZONTAL;
+    }
+    
+    public static void ButtonsNumbers(){
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 1;
+        c.weighty = 1;
+        c.gridwidth = 1;
         
         button = new JButton("%");
         c.gridx = 0;
-        c.gridy = 0;
-        c.weightx = 0.5;
+        c.gridy = 2;
         frame.add(button, c);
         
         button = new JButton("/");
         c.gridx = 1;
+        c.gridy = 2;
         frame.add(button, c);
         
         button = new JButton("*");
@@ -87,7 +94,7 @@ public class GridBag {
         
         button = new JButton("7");
         c.gridx = 0;
-        c.gridy = 1;
+        c.gridy = 3;
         frame.add(button, c);
         
         button = new JButton("8");
@@ -100,7 +107,7 @@ public class GridBag {
         
         button = new JButton("4");
         c.gridx = 0;
-        c.gridy = 2;
+        c.gridy = 4;
         frame.add(button, c);
         
         button = new JButton("5");
@@ -113,7 +120,7 @@ public class GridBag {
         
         button = new JButton("1");
         c.gridx = 0;
-        c.gridy = 3;
+        c.gridy = 5;
         frame.add(button, c);
         
         button = new JButton("2");
@@ -126,7 +133,7 @@ public class GridBag {
         
         button = new JButton("0");
         c.gridx = 0;
-        c.gridy = 4;
+        c.gridy = 6;
         c.gridwidth = 2;
         frame.add(button, c);
         
@@ -136,16 +143,64 @@ public class GridBag {
         frame.add(button, c);
         
         button = new JButton("+");
-        c.fill = GridBagConstraints.BOTH;
         c.gridx = 3;
-        c.gridy = 1;
+        c.gridy = 3;
         c.gridheight = 2;
         frame.add(button, c);
         
         button = new JButton("=");
-        c.gridy = 3;
+        c.gridy = 5;
+        frame.add(button, c);
+    }
+    
+    public static void ButtonsControl(){
+        button = new JButton("CLEAR");
+        c.fill = GridBagConstraints.BOTH;
+        c.gridx = 4;
+        c.gridy = 2;
+        c.gridheight = 2;
         frame.add(button, c);
         
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        
+        button = new JButton("(");
+        c.gridy = 4;
+        frame.add(button, c);
+        
+        button = new JButton(")");
+        c.gridy = 5;
+        frame.add(button, c);
+        
+        button = new JButton("+/-");
+        c.gridy = 6;
+        frame.add(button, c);
+    }
+    
+    public static void TextDisplay(){
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 1;
+        c.weighty = 1;
+        
+        JTextField mathFormula = new JTextField();
+        c.gridx = 0;
+        c.gridy = 0;
+        c.gridheight = 1;
+        c.gridwidth = 5;
+        frame.add(mathFormula, c);
+        
+        JTextField mathAnswer = new JTextField();
+        c.gridy = 1;
+        frame.add(mathAnswer, c);
+    }
+    
+    public static void CALC(){
+        SetFrameSettings();
+        
+        TextDisplay();
+        ButtonsNumbers();
+        ButtonsControl();
+           
         frame.setVisible(true);
     }
 }
