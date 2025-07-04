e9b77b865101c7f8d9750e898f59feb0cb0f69c2
prox3m <doronindima997@gmail.com>
2025-07-04 13:08:49 +0500

    Buttons connected

--- src/org/javalearn/GridBag.java
+++ src/org/javalearn/GridBag.java
@@ -2,64 +2,24 @@ package org.javalearn;
 
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
 
 public class GridBag {
-    public static void GUI(){
-        
-        final JFrame frame = new JFrame("Test");
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setSize(400, 400);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        
-        JButton button;
-        frame.setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        
-        button = new JButton("Button 1");
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.weightx = 0.4;
-        frame.add(button, gbc);
-        
-        button = new JButton("Button 2");
-        gbc.gridx = 1;
-        gbc.gridy = 0;
-        gbc.weightx = 0.4;
-        frame.add(button, gbc);
-        
-        button = new JButton("Button 3");
-        gbc.gridx = 2;
-        gbc.gridy = 0;
-        gbc.weightx = 0.4;
-        frame.add(button, gbc);
-        
-        button = new JButton("Button 4");
-        gbc.ipady = 40;
-        gbc.gridwidth = 3;
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        gbc.weightx = 0.0;
-        frame.add(button, gbc);
-        
-        button = new JButton("Button 5");
-        gbc.ipady = 0;
-        gbc.weighty = 1;
-        gbc.anchor = GridBagConstraints.PAGE_END;
-        gbc.gridwidth = 2;
-        gbc.gridx = 1;
-        gbc.gridy = 2;
-        frame.add(button, gbc);
-        
-        frame.setVisible(true);
-    }
     
     public static final JFrame frame = new JFrame("Калькулятор");
-    
+    public static final JTextField mathFormula = new JTextField();
+    public static final JTextField mathAnswer = new JTextField();
     public static JButton button;
     public static final GridBagConstraints c = new GridBagConstraints();
+    
+    public static void ActionAdd(String action){
+        String formula = mathFormula.getText();
+        mathFormula.setText(formula + action);
+    }
+    public static void DigitAdd(String digit){
+        String formula = mathFormula.getText();
+        mathFormula.setText(formula + digit);
+    }
     
     public static void SetFrameSettings(){
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -78,79 +38,176 @@ public class GridBag {
         c.gridx = 0;
         c.gridy = 2;
         frame.add(button, c);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                ActionAdd("%");
+            }
+        });
         
         button = new JButton("/");
         c.gridx = 1;
         c.gridy = 2;
         frame.add(button, c);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                ActionAdd("/");
+            }
+        });
         
         button = new JButton("*");
         c.gridx = 2;
         frame.add(button, c);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                ActionAdd("*");
+            }
+        });
         
         button = new JButton("-");
         c.gridx = 3;
         frame.add(button, c);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                ActionAdd("-");
+            }
+        });
         
         button = new JButton("7");
         c.gridx = 0;
         c.gridy = 3;
         frame.add(button, c);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                DigitAdd("7");
+            }
+        });
         
         button = new JButton("8");
         c.gridx = 1;
         frame.add(button, c);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                DigitAdd("8");
+            }
+        });
         
         button = new JButton("9");
         c.gridx = 2;
         frame.add(button, c);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                DigitAdd("9");
+            }
+        });
         
         button = new JButton("4");
         c.gridx = 0;
         c.gridy = 4;
         frame.add(button, c);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                DigitAdd("4");
+            }
+        });
         
         button = new JButton("5");
         c.gridx = 1;
         frame.add(button, c);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                DigitAdd("5");
+            }
+        });
         
         button = new JButton("6");
         c.gridx = 2;
         frame.add(button, c);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                DigitAdd("6");
+            }
+        });
         
         button = new JButton("1");
         c.gridx = 0;
         c.gridy = 5;
         frame.add(button, c);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                DigitAdd("1");
+            }
+        });
         
         button = new JButton("2");
         c.gridx = 1;
         frame.add(button, c);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                DigitAdd("2");
+            }
+        });
         
         button = new JButton("3");
         c.gridx = 2;
         frame.add(button, c);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                DigitAdd("3");
+            }
+        });
         
         button = new JButton("0");
         c.gridx = 0;
         c.gridy = 6;
         c.gridwidth = 2;
         frame.add(button, c);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                DigitAdd("0");
+            }
+        });
         
         button = new JButton(",");
         c.gridx = 2;
         c.gridwidth = 1;
         frame.add(button, c);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                DigitAdd(".");
+            }
+        });
         
         button = new JButton("+");
         c.gridx = 3;
         c.gridy = 3;
         c.gridheight = 2;
         frame.add(button, c);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                ActionAdd("+");
+            }
+        });
         
         button = new JButton("=");
         c.gridy = 5;
         frame.add(button, c);
+
     }
     
     public static void ButtonsControl(){
@@ -160,6 +217,13 @@ public class GridBag {
         c.gridy = 2;
         c.gridheight = 2;
         frame.add(button, c);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                mathFormula.setText("");
+                mathAnswer.setText("");
+            }
+        });
         
         c.gridwidth = 1;
         c.gridheight = 1;
@@ -167,10 +231,22 @@ public class GridBag {
         button = new JButton("(");
         c.gridy = 4;
         frame.add(button, c);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                ActionAdd("(");
+            }
+        });
         
         button = new JButton(")");
         c.gridy = 5;
         frame.add(button, c);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                ActionAdd(")");
+            }
+        });
         
         button = new JButton("+/-");
         c.gridy = 6;
@@ -182,7 +258,6 @@ public class GridBag {
         c.weightx = 1;
         c.weighty = 1;
         
-        JTextField mathFormula = new JTextField();
         mathFormula.setHorizontalAlignment(SwingConstants.RIGHT);
         c.gridx = 0;
         c.gridy = 0;
@@ -190,10 +265,8 @@ public class GridBag {
         c.gridwidth = 5;
         frame.add(mathFormula, c);
         
-        JTextField mathAnswer = new JTextField();
         mathAnswer.setHorizontalAlignment(SwingConstants.RIGHT);
         mathAnswer.setEditable(false);
-        
         c.gridy = 1;
         frame.add(mathAnswer, c);
     }
--- src/org/javalearn/Main.java
+++ src/org/javalearn/Main.java
@@ -6,25 +6,7 @@ import javax.swing.*;
 public class Main {
     
     public static void main(final String[] args) {
-        
         GridBag g = new GridBag();
-        //g.GUI();
         g.CALC();
-        
-        /*final JFrame frame = new JFrame("Frame on java");
-        JPanel buttonsPanel = new JPanel();
-        
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setSize(350, 500);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        
-        JButton button = new JButton("Кнопка");
-        buttonsPanel.add(button);
-        
-        frame.getContentPane().add(BorderLayout.SOUTH, buttonsPanel);
-        
-        frame.setVisible(true);*/
     }
-    
 }
